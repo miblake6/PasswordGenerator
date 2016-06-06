bdd32de7f8948e208716c570ef0679109c5c64aa
Macbrine <mmacneil@vols.utk.edu>
2016-06-06 18:10:07 -0400

    Add files via upload

---
+++ PasswordGenerator.cpp
@@ -0,0 +1,25 @@
+//============================================================================
+// Name        : PasswordGenerator.cpp
+// Author      : Macbrine
+// Version     :
+// Copyright   : Don't do it
+// Description : Password Generator
+//============================================================================
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+	unsigned int pw_length = 0;
+	cout << "Input desired password length: ";
+	cin >> pw_length;
+	string pw(pw_length, 'a');
+	for(auto& digit : pw)
+		digit = static_cast<char>(rand()%255);
+	cout << "Your pw is " << pw << endl;
+
+	return 0;
+}
+
